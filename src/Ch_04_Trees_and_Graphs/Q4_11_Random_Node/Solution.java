a3ad5df45257ea0558da59b165b47d3bbed0b9d8
Ilya-Hlebik <felix.2894@mail.ru>
2023-04-04 22:26:12 -0500

    4.11 cont. + 4.12

--- src/Ch_04_Trees_and_Graphs/Q4_11_Random_Node/Solution.java
+++ src/Ch_04_Trees_and_Graphs/Q4_11_Random_Node/Solution.java
@@ -1,6 +1,8 @@
 package Ch_04_Trees_and_Graphs.Q4_11_Random_Node;
 
-import CtCILibrary.TreeNode;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
 
@@ -21,13 +23,79 @@ public class Solution {
         }
     }
 }
+
 class Tree {
+    TreeNode root = null;
+    int size = 0;
 
     public void insertInOrder(int x) {
-
+        if (root == null) {
+            root = new TreeNode(x);
+        } else {
+            root.insertInOrder(x);
+        }
+        size++;
     }
 
+
     public TreeNode getRandomNode() {
-        return null;
+        AtomicInteger random = new AtomicInteger(new Random().nextInt(size));
+        return inOrderWithStop(root, random, new AtomicInteger(0));
+    }
+
+    private TreeNode inOrderWithStop(TreeNode root, AtomicInteger random, AtomicInteger current) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode treeNode = inOrderWithStop(root.left, random, current);
+        if (treeNode != null) {
+            return treeNode;
+        }
+        if (random.get() == current.get()) {
+            return root;
+        } else {
+            current.incrementAndGet();
+        }
+        return inOrderWithStop(root.right, random, current);
+    }
+}
+
+class TreeNode {
+    public TreeNode right;
+    public TreeNode left;
+    int data;
+
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public TreeNode find(int d) {
+        if (d == data) {
+            return this;
+        }
+        if (d <= data) {
+            return left != null ? left.find(d) : null;
+        } else {
+            return right != null ? right.find(d) : null;
+        }
+    }
+
+
+    public void insertInOrder(int x) {
+        if (this.data >= x) {
+            if (left == null) {
+                left = new TreeNode(x);
+            } else {
+                left.insertInOrder(x);
+            }
+
+        } else {
+            if (right == null) {
+                right = new TreeNode(x);
+            } else {
+                right.insertInOrder(x);
+            }
+        }
     }
 }
