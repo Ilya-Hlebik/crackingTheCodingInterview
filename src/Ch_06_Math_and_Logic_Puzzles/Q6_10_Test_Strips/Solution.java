efcdafc6e3a7b6df69aaa54f78fb3127d5138aa7
Ilya-Hlebik <felix.2894@mail.ru>
2023-04-25 22:49:48 -0500

    6.10 cont

--- src/Ch_06_Math_and_Logic_Puzzles/Q6_10_Test_Strips/Solution.java
+++ src/Ch_06_Math_and_Logic_Puzzles/Q6_10_Test_Strips/Solution.java
@@ -1,24 +1,89 @@
 package Ch_06_Math_and_Logic_Puzzles.Q6_10_Test_Strips;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     static int currentPoisoned = 1;
-    int total = 10000;
+    static int totalTo = 10000;
+    static int totalFrom = 0;
 
     public static void main(String[] args) {
-        for (int i = 1; i < 10000; i++) {
+        Solution solution = new Solution();
+        for (int i = 115; i < 10000; i++) {
             currentPoisoned = i;
+            totalTo = 10000;
+            totalFrom = 0;
+            System.out.println(solution.amountOfDays());
         }
     }
 
     int amountOfDays() {
+        int countOfDays = 0;
         int countOfTests = 10;
-        int each = total / countOfTests;
-        while (total !=0){
-
+        while (countOfTests > 0) {
+            int each = (totalTo - totalFrom) / countOfTests;
+            if (each == 0){
+                each = 1;
+            }
+            List<Pair> tests = new ArrayList<>();
+            for (int i = 0; i < countOfTests && totalTo != 0; i++) {
+                int currentTest;
+                Pair pair = new Pair();
+                if (i+1 == countOfTests) {
+                    pair.setTo(totalTo);
+                    pair.setFrom(totalFrom);
+                } else {
+                    currentTest = totalTo - each;
+                    pair.setTo(totalTo);
+                    totalTo  = currentTest;
+                    pair.setFrom(totalTo);
+                }
+                tests.add(pair);
+                if (pair.getFrom() == totalFrom){
+                    break;
+                }
+            }
+            countOfDays += 7;
+            for (Pair test : tests) {
+                boolean poisoned = isPoisoned(test);
+                if (poisoned && test.getFrom() +1 != test.getTo()) {
+                    countOfTests--;
+                    totalFrom = test.getFrom();
+                    totalTo = test.getTo();
+                    break;
+                } else if (poisoned) {
+                    System.out.print("currentPoisoned: " + test.getTo() + " amountOfDays:");
+                    return countOfDays;
+                }
+            }
         }
+        return countOfDays;
     }
 
-    boolean isPoisoned(int fromRange, int toRange) {
-        return fromRange >= currentPoisoned && toRange <= currentPoisoned;
+    boolean isPoisoned(Pair pair) {
+        return pair.getFrom() < currentPoisoned && pair.getTo() >= currentPoisoned;
+    }
+}
+
+class Pair {
+    int from;
+    int to;
+
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
     }
 }
