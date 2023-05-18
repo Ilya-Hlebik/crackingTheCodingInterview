b9c87015ef7db92f994101987c7c5bd787eeba11
Ilya-Hlebik <felix.2894@mail.ru>
2023-05-17 22:16:39 -0500

    7.7 begin

---
+++ src/Ch_07_Object_Oriented_Design/Q7_07_Chat_Server/UserManager.java
@@ -0,0 +1,14 @@
+package Ch_07_Object_Oriented_Design.Q7_07_Chat_Server;
+
+import java.util.List;
+
+public class UserManager {
+    private List<User> userList;
+
+    public List<User> getUserList() {
+        return userList;
+    }
+    private void addUser(User user){
+
+    }
+}
