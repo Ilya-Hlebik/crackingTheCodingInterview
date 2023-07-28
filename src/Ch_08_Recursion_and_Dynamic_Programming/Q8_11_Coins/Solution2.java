445dd8ad7d8ce31fe5f35af15fb1203ff4f5cda3
Ilya-Hlebik <felix.2894@mail.ru>
2023-07-27 22:32:22 -0500

    8.11 cont

--- src/Ch_08_Recursion_and_Dynamic_Programming/Q8_11_Coins/Solution2.java
+++ src/Ch_08_Recursion_and_Dynamic_Programming/Q8_11_Coins/Solution2.java
@@ -1,20 +1,24 @@
 package Ch_08_Recursion_and_Dynamic_Programming.Q8_11_Coins;
 
-public class Solution {
+public class Solution2 {
     public static void main(String[] args) {
         for (int i = 0; i < 201; i++) {
             int[] denoms = {25, 10, 5, 1};
             int waysA = makeChange(i, denoms);
-            System.out.println(i + " " +waysA);
+            System.out.println(i + " " + waysA);
         }
 
     }
 
     private static int makeChange(int i, int[] denoms) {
-        return makeChange(i, denoms, 0);
+        int[][] memo = new int[i + 1][denoms.length];
+        return makeChange(i, denoms, 0, memo);
     }
 
-    private static int makeChange(int total, int[] denoms, int index) {
+    private static int makeChange(int total, int[] denoms, int index, int[][] memo) {
+        if (memo[total][index] != 0) {
+            return memo[total][index];
+        }
         int coin = denoms[index];
         if (index == denoms.length - 1) {
             return total % coin == 0 ? 1 : 0;
@@ -22,8 +26,9 @@ public class Solution {
 
         int ways = 0;
         for (int amount = 0; amount <= total; amount += coin) {
-            ways += makeChange(total - amount, denoms, index + 1);
+            ways += makeChange(total - amount, denoms, index + 1, memo);
         }
+        memo[total][index] = ways;
 
         return ways;
     }
