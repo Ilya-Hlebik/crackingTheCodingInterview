c01a5e29c1a6b84c7db0320dc14f6c7baefc5538
Ilya-Hlebik <felix.2894@mail.ru>
2023-08-08 22:16:20 -0500

    9.1

---
+++ src/Ch_09_Scalability_and_Memory_Limits/Q9_01_Stock_Data/Solution.java
@@ -0,0 +1,11 @@
+package Ch_09_Scalability_and_Memory_Limits.Q9_01_Stock_Data;
+
+public class Solution {
+    public static void main(String[] args) {
+      /*
+      * 1) Store data as plain text file, then use FTP
+      * 2) Use sql, see Diagram
+      * 3) Use XML format for each stock
+      **/
+    }
+}
