bde230e3625b0f599691a15fd668d9d5562d70a9
Ilya-Hlebik <felix.2894@mail.ru>
2023-08-15 22:44:45 -0500

    9.5, 9.6

--- src/Ch_09_Scalability_and_Memory_Limits/Q9_05_Cache/Solution.java
+++ src/Ch_09_Scalability_and_Memory_Limits/Q9_05_Cache/Solution.java
@@ -1,4 +1,35 @@
 package Ch_09_Scalability_and_Memory_Limits.Q9_05_Cache;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
+    private Map<String, List<String>> cashQueries = new HashMap<>();
+
+    public List<String> processSearch(String query) {
+        return getSearchFromCash(query);
+    }
+
+    public void updateData(List<String> newData, String query) {
+        //save
+        if (cashQueries.containsKey(query)) {
+            cashQueries.remove(query);
+        }
+    }
+
+    public List<String> getSearchFromCash(String query) {
+        if (cashQueries.containsKey(query)) {
+            return cashQueries.get(query);
+        } else {
+            List<String> searchFromDb = getSearchFromDb(query);
+            cashQueries.put(query, searchFromDb);
+            return searchFromDb;
+        }
+    }
+
+    private List<String> getSearchFromDb(String query) {
+
+        return null;
+    }
 }
---
+++ src/Ch_09_Scalability_and_Memory_Limits/Q9_06_Sales_Rank/Solution.java
@@ -0,0 +1,19 @@
+package Ch_09_Scalability_and_Memory_Limits.Q9_06_Sales_Rank;
+
+public class Solution {
+    public static void main(String[] args) {
+/*
+* 1)Assumptions
+* 2)Do data dump every 1 hour
+* 3)Dump data in column database, e.g. Druid
+* 4)Each line should represent one item with column:
+*  ItemId ItemName Total Sales Mon, Tue, Wed, Thu, Fri, Sat, San.
+*   Apply sort on this of fly
+* 5)Add cache on top of this.
+*
+* Alternative solution. Use regular database instead with cache
+*
+*
+* */
+    }
+}
