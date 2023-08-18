58d8bda18839de9ac2a0a7ecb29c65e5d77ffa93
Ilya-Hlebik <felix.2894@mail.ru>
2023-08-17 22:32:47 -0500

    10 merge + quick

---
+++ src/Ch_10/_Sorting_and_Searching/Quick_sort/Solution.java
@@ -0,0 +1,46 @@
+package Ch_10._Sorting_and_Searching.Quick_sort;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] array = {4, 3, 6, 7, 8, 2, 3, 5, 8, 9, 1, 2};
+        solution.quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public void quickSort(int array[], int left, int right) {
+        int index = partition(array, left, right);
+        if (left < index - 1) {
+            quickSort(array, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(array, index, right);
+        }
+    }
+
+    private int partition(int[] array, int left, int right) {
+        int pivot = array[left + (right - left) / 2];
+        while (left <= right) {
+            while (array[left] < pivot) {
+                left++;
+            }
+            while (array[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
