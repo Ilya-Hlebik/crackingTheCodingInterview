ecb52991c9ecb6d0eba5223016e6e7974e339cf9
Ilya-Hlebik <felix.2894@mail.ru>
2023-11-22 21:33:08 -0600

    rep 9.6

---
+++ src/Ch_09_Scalability_and_Memory_Limits/Q9_06_Sales_Rank/run2/Solution.java
@@ -0,0 +1,17 @@
+package Ch_09_Scalability_and_Memory_Limits.Q9_06_Sales_Rank.run2;
+
+public class Solution {
+    public static void main(String[] args) {
+        /*
+         * 1)Assumptions
+         * 2)Do data dump of recent purchase every 1 hour or until it's reach certain amount.
+         * 3)Save computed data in database
+         * 4)Each line should represent one item with column:
+         *  ItemId CategoryId ItemName Total Sales Mon, Tue, Wed, Thu, Fri, Sat, San.
+         *   Apply sort on this of fly
+         * 5)Add cache on top of this.
+         *
+         *
+         * */
+    }
+}
